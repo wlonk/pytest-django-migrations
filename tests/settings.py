fc5cc95430a3001ed209f5e1aac46053aa30fb91
Kit La Touche <kit@transneptune.net>
2019-02-26 15:10:20 +0000

    Use dj-database-url for tests

--- tests/settings.py
+++ tests/settings.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8
+import dj_database_url
+
+
 DEBUG = True
 USE_TZ = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "a secret key for testing"
 
-DATABASES = {
-    "default": {
-        "NAME": "pytedjmi",
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "ATOMIC_REQUESTS": False,
-        "AUTOCOMMIT": True,
-    }
-}
+DATABASES = {"default": dj_database_url.config(default="postgres:///pytedjmi")}
 
 ROOT_URLCONF = "tests.urls"
 
